b042dfcafd501f53de0cc79a3794c93ad90ad233
Caleb Bacon <jc.bacon@hotmail.com>
2015-05-08 12:00:50 -0400

    Splash Screen / Listview
    Splash Screen using AsyncTask / Main activity Listview

--- app/src/main/java/com/example/bacons/newbnotepad/NewNoteActivity.java
+++ app/src/main/java/com/example/bacons/newbnotepad/NewNoteActivity.java
@@ -6,26 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class FirstScreen extends ActionBarActivity {
+public class NewNoteActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_screen);
-    }
-
-
-    @Override
-    protected void onDestroy() {
-
-        super.onDestroy();
-
+        setContentView(R.layout.activity_new_note);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_first_screen, menu);
+        getMenuInflater().inflate(R.menu.menu_new_note, menu);
         return true;
     }
 
